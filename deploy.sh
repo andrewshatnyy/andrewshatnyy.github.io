5687704c5123d97c3aa9ef9927108ee1e78c0fac
Andrew Shatnyy <AndrewShatnyy@gmail.com>
2015-04-14 22:56:24 -0700

    updated deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+echo =========================
+echo GZIP All Html, css and js
+echo =========================
+
+find _site -iname '*.xml' -exec gzip -n {} +
+find _site -iname '*.html' -exec gzip -n {} +
+find _site -iname '*.js' -exec gzip -n {} +
+find _site -iname '*.css' -exec gzip -n {} +
+
 cd _site &&
 zip -r ../site.zip ./* &&
-cd ../ &&
+cd ../
 scp site.zip shatnyy:www/ &&
-ssh shatnyy "cd www && rm -rf andrewshatnyy && unzip site.zip -d andrewshatnyy && rm site.zip"
+scp .htaccess shatnyy:www/andrewshatnyy &&
+ssh shatnyy "cd www && rm -rf andrewshatnyy && unzip site.zip -d andrewshatnyy && rm site.zip" &&
+rm site.zip
